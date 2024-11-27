63b2418fc5cd55f92970acc592eaa9c7ba4dd5c7
joanne4859 <joannw8@uci.edu>
2024-11-26 20:03:53 -0800

    fixed organization and stuff

--- src/main.cpp
+++ src/main.cpp
@@ -7,18 +7,18 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-char ssid[] = "Ucinet Mobile Access"; 
-char pass[] = ""; 
-// Name of the server we want to connect to
-const char kHostname[] = "worldtimeapi.org";
-// Path to download (this is the bit after the hostname in the URL
-// that you want to download
-const char kPath[] = "/api/timezone/Europe/London.txt";
+// char ssid[] = "Ucinet Mobile Access"; 
+// char pass[] = ""; 
+// // Name of the server we want to connect to
+// const char kHostname[] = "worldtimeapi.org";
+// // Path to download (this is the bit after the hostname in the URL
+// // that you want to download
+// const char kPath[] = "/api/timezone/Europe/London.txt";
 
-// Number of milliseconds to wait without receiving any data before we give up
-const int kNetworkTimeout = 30 * 1000;
-// Number of milliseconds to wait if no data is available before trying again
-const int kNetworkDelay = 1000;
+// // Number of milliseconds to wait without receiving any data before we give up
+// const int kNetworkTimeout = 30 * 1000;
+// // Number of milliseconds to wait if no data is available before trying again
+// const int kNetworkDelay = 1000;
 
 // #define ACCEL_PIN 
 // #define TEMP_PIN
@@ -61,21 +61,21 @@ void setup(){
   Serial.print("Threshold:");
   Serial.println(threshold);
   Serial.println("Calibration complete.");
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
+  // Serial.print("Connecting to ");
+  // Serial.println(ssid);
 
-  WiFi.begin(ssid, pass);
+  // WiFi.begin(ssid, pass);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-  Serial.println("MAC address: ");
-  Serial.println(WiFi.macAddress());
+  // while (WiFi.status() != WL_CONNECTED) {
+  //   delay(500);
+  //   Serial.print(".");
+  // }
+  // Serial.println("");
+  // Serial.println("WiFi connected");
+  // Serial.println("IP address: ");
+  // Serial.println(WiFi.localIP());
+  // Serial.println("MAC address: ");
+  // Serial.println(WiFi.macAddress());
   Serial.println("Place finger on sensor. Monitoring will begin in 10 seconds.");
 }
 
@@ -129,75 +129,59 @@ void loop(){
       startTime = millis();
           int err = 0;
 
-      WiFiClient c;
-      HttpClient http(c);
+  //     WiFiClient c;
+  //     HttpClient http(c);
 
-      char result[20];
-      sprintf (result, "%.1d", BPM);
-      char urlParam [30];
-      strcpy(urlParam, "/?var=");
-      strcat (urlParam, result);
+  //     char result[20];
+  //     sprintf (result, "%.1d", BPM);
+  //     char urlParam [30];
+  //     strcpy(urlParam, "/?var=");
+  //     strcat (urlParam, result);
 
-      err = http.get("3.145.7.151", 5000, urlParam, NULL);
-
-      
-
-      if (err == 0) {
-        Serial.println("startedRequest ok");
-
-        err = http.responseStatusCode();
-        if (err >= 0) {
-          Serial.print("Got status code: ");
-          Serial.println(err);
-          
-          // Usually you'd check that the response code is 200 or a
-          // similar "success" code (200-299) before carrying on,
-          // but we'll print out whatever response we get
+  //     err = http.get("3.145.7.151", 5000, urlParam, NULL);
 
 
-          err = http.skipResponseHeaders();
-          if (err >= 0) {
-            int bodyLen = http.contentLength();
-            Serial.print("Content length is: ");
-            Serial.println(bodyLen);
-            Serial.println();
-            Serial.println("Body returned follows:");
+  //   if (err == 0) {
+  //     Serial.println("startedRequest ok");
+  //     err = http.responseStatusCode();
+  //     if (err >= 0) {
+  //       Serial.print("Got status code: ");
+  //       Serial.println(err);
+  //       err = http.skipResponseHeaders();
+  //       if (err >= 0) {
+  //         int bodyLen = http.contentLength();
+  //         Serial.print("Content length is: ");
+  //         Serial.println(bodyLen);
+  //         Serial.println();
+  //         Serial.println("Body returned follows:");
 
-            // Now we've got to the body, so we can print it out
-            unsigned long timeoutStart = millis();
-            char c;
-            // Whilst we haven't timed out & haven't reached the end of the body
-            while ((http.connected() || http.available()) &&
-                    ((millis() - timeoutStart) < kNetworkTimeout)) {
-              if (http.available()) {
-                c = http.read();
-                // Print out this character
-                Serial.print(c);
+  //         unsigned long timeoutStart = millis();
+  //         char c;
+  //         while ((http.connected() || http.available()) &&
+  //           ((millis() - timeoutStart) < kNetworkTimeout)) {
+  //             if (http.available()) {
+  //               c = http.read();
+  //               Serial.print(c);
 
-                bodyLen--;
-                // We read something, reset the timeout counter
-                timeoutStart = millis();
-              } else {
-                // We haven't got any data, so let's pause to allow some to
-                // arrive
-                delay(kNetworkDelay);
-              }
-            }
-          } else {
-            Serial.print("Failed to skip response headers: ");
-            Serial.println(err);
-          }
-        } else {
-          Serial.print("Getting response failed: ");
-          Serial.println(err);
-        }
-      } else {
-        Serial.print("Connect failed: ");
-        Serial.println(err);
-      }
-      http.stop();    
-
-      
+  //               bodyLen--;
+  //               timeoutStart = millis();
+  //             } else {
+  //               delay(kNetworkDelay);
+  //         }
+  //       }
+  //     } else {
+  //       Serial.print("Failed to skip response headers: ");
+  //       Serial.println(err);
+  //     }
+  //   } else {
+  //     Serial.print("Getting response failed: ");
+  //     Serial.println(err);
+  //   }
+  // } else {
+  //   Serial.print("Connect failed: ");
+  //   Serial.println(err);
+  // }
+  // http.stop();
     }
   }
 }
