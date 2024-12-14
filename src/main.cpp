0669dda635247dac69b53a18fc86f08f5c8094ed
joanne4859 <joannw8@uci.edu>
2024-12-13 22:59:56 -0800

    Organized code into separate functions, debugged outputs to website

--- src/main.cpp
+++ src/main.cpp
@@ -10,83 +10,64 @@
 #include "DHT20.h"
 #include <SparkFunLSM6DSO.h>
 
-char ssid[] = "Panda Express"; 
-char pass[] = "Orangechicken315"; 
-// // Name of the server we want to connect to
-// const char kHostname[] = "worldtimeapi.org";
-// // Path to download (this is the bit after the hostname in the URL
-// // that you want to download
-// const char kPath[] = "/api/timezone/Europe/London.txt";
-
-// // Number of milliseconds to wait without receiving any data before we give up
-const int kNetworkTimeout = 30 * 1000;
-// // Number of milliseconds to wait if no data is available before trying again
-const int kNetworkDelay = 1000;
-
-#define STEP_PIN_SDA 25
-#define STEP_PIN_SCL 26
-
-#define TEMP_PIN_SDA 21
-#define TEMP_PIN_SCL 22
-
+// Pins and Constants
+#define TEMP_PIN_SDA 22
+#define TEMP_PIN_SCL 21
+//#define STEP_PIN_SDA 10
+//#define STEP_PIN_SCL 12
 #define HEART_PIN 32
+
+char ssid[] = "Panda Express"; 
+char pass[] = "Orangechicken315";
+
+void connectToWiFi();
+void calibratePulseSensor();
+void calibrateStepSensor();
+void readPulseSensor();
+void readTemperature();
+void countSteps();
+void sendDataToServer();
+
 
 DHT20 DHT;
 LSM6DSO myIMU;
 
-int step = 0;
-int count = 0;
-
-int offset = 0;
-unsigned long lastBeatTime = 0;
-int BPM = 0;
-int beatCount = 0;
+int step = 0, count = 0, beatCount = 0, BPM = 0, threshold = 0;
 bool startMonitor = false;
 const int monitoringPeriod = 10000;
-int threshold = 0;
+int offset = 0;
+unsigned long lastBeatTime = 0;
 
 void setup(){
   Serial.begin(9600);
   delay(1000);
-  
-  pinMode(HEART_PIN, INPUT);
 
   Wire.begin(TEMP_PIN_SDA, TEMP_PIN_SCL);
-  Wire.begin(STEP_PIN_SDA, STEP_PIN_SCL);
+  DHT.begin();
+  Serial.println("Hello World!");
+  // if (!myIMU.begin() || !myIMU.initialize(BASIC_SETTINGS)) {
+  //   Serial.println("Failed to initialize LSM6DSO!");
+  //   while (1);
+  // }
 
-  // **************************************************************
+  // Connect to WiFi
+  connectToWiFi();
 
-  count = 0;
+  // Calibrate Sensors
+  calibratePulseSensor();
+  //calibrateStepSensor();
 
-  Wire.begin();
-  delay(10);
-  if( myIMU.begin() )
-    Serial.println("Ready.");
-  else { 
-    Serial.println("Could not connect to IMU.");
-    Serial.println("Freezing");
-  }
+}
 
-   if( myIMU.initialize(BASIC_SETTINGS) )
-    Serial.println("Loaded Settings.");
-
-  int min1 = myIMU.readFloatGyroX();
-  int min2 = myIMU.readFloatGyroY();
-  int min = sqrt(pow(min1,2) + pow(min2,2));
-  Serial.println("MOVE");
-  delay(5000);
-
-  int max1= myIMU.readFloatGyroX();
-  int max2 = myIMU.readFloatGyroY();
-  int max = sqrt(pow(max1,2) + pow(max2,2));
-  step = abs(max - min);
-  Serial.println("Starting loop");
+void loop() {
+  readPulseSensor();
+  readTemperature();
+  //countSteps();
+  sendDataToServer();
   delay(1000);
+}
 
-  // We start by connecting to a WiFi network
-  delay(1000);
-  Serial.println();
-  Serial.println();
+void connectToWiFi() {
   Serial.print("Connecting to ");
   Serial.println(ssid);
 
@@ -102,12 +83,9 @@ void setup(){
   Serial.println(WiFi.localIP());
   Serial.println("MAC address: ");
   Serial.println(WiFi.macAddress());
+}
 
-  Wire.begin();
-  DHT.begin();
-
-  // **************************************************************
-
+void calibratePulseSensor() {
   Serial.println("Pulse sensor connected!"); 
   Serial.println("Calibrating pulse sensor...");
 
@@ -116,7 +94,6 @@ void setup(){
   int min_pulse = 4095;
   int max_pulse  = 0;
   int desired = millis() + 2000;
-  //for (int i = 0; i < numReadings; i++){   // Take avg pulse rate w/o finger
   while (millis() < desired){
       reading = analogRead(HEART_PIN);
       if (reading < min_pulse){
@@ -130,27 +107,24 @@ void setup(){
   Serial.print("Threshold:");
   Serial.println(threshold);
   Serial.println("Calibration complete.");
-  // Serial.print("Connecting to ");
-  // Serial.println(ssid);
-
-  // WiFi.begin(ssid, pass);
-
-  // while (WiFi.status() != WL_CONNECTED) {
-  //   delay(500);
-  //   Serial.print(".");
-  // }
-  // Serial.println("");
-  // Serial.println("WiFi connected");
-  // Serial.println("IP address: ");
-  // Serial.println(WiFi.localIP());
-  // Serial.println("MAC address: ");
-  // Serial.println(WiFi.macAddress());
   Serial.println("Place finger on sensor. Monitoring will begin in 10 seconds.");
 }
 
-void loop(){
-  
+void calibrateStepSensor() {
+  int min1 = myIMU.readFloatGyroX();
+  int min2 = myIMU.readFloatGyroY();
+  int min = sqrt(pow(min1,2) + pow(min2,2));
+  Serial.println("MOVE");
+  delay(5000);
 
+  int max1= myIMU.readFloatGyroX();
+  int max2 = myIMU.readFloatGyroY();
+  int max = sqrt(pow(max1,2) + pow(max2,2));
+  step = abs(max - min);
+  delay(1000);
+}
+
+void readPulseSensor() {
   static unsigned long startTime = millis(); // Time when monitoring started
   static int heartSignal1 = 0;              // Previous signal value
   static int heartSignal2 = 0;              // Current signal value
@@ -188,7 +162,7 @@ void loop(){
 
     if (currTime - startTime >= monitoringPeriod) {
       BPM = beatCount * 6;
-
+      sendDataToServer();
       Serial.print("Total Beats: ");
       Serial.println(beatCount);
       Serial.print("Heart Rate: ");
@@ -202,29 +176,16 @@ void loop(){
 
     }
   }
+}
 
-  // **************************************************************
-  // Temperature
+void readTemperature() {
   DHT.read();
-
-  int err = 0;
-
-  WiFiClient c;
-  HttpClient http(c);
-
   float temperature = DHT.getTemperature();
+  Serial.print("Temperature: ");
+  Serial.println(temperature);
+}
 
-  char result[20];
-  sprintf (result, "%.1f,%.1f", temperature);
-  char urlParam [30];
-  strcpy(urlParam, "/?var=");
-  strcat (urlParam, result);
-
-  err = http.get("3.145.7.151", 5000, urlParam, NULL);
-
-  // **************************************************************
-  // Step count
-
+void countSteps() {
   int prev1= myIMU.readFloatGyroX();
   int prev2 = myIMU.readFloatGyroY();
   int prev = sqrt(pow(prev1,2) + pow(prev2,2));
@@ -240,39 +201,16 @@ void loop(){
   Serial.println(count);
   std::string print_count = std::to_string(count);
   delay(20);
+}
 
-  // **************************************************************
-  if (err == 0) {
-    Serial.println("startedRequest ok");
+void sendDataToServer() {
+  WiFiClient c;
+  HttpClient http(c);
 
-    err = http.responseStatusCode();
-    if (err >= 0) {
-      Serial.print("Got status code: ");
-      Serial.println(err);
+  // Format and send data
+  char urlParam[50];
+  sprintf(urlParam, "/update_step?temp=%.1f&bpm=%d", DHT.getTemperature(), beatCount * 6);
 
-      err = http.skipResponseHeaders();
-      if (err >= 0) {
-        int bodyLen = http.contentLength();
-        Serial.print("Content length is: ");
-        Serial.println(bodyLen);
-        Serial.println();
-        Serial.println("Body returned follows:");
-
-        // Now we've got to the body, so we can print it out
-        unsigned long timeoutStart = millis();
-        char c;
-
-      } else {
-        Serial.print("Failed to skip response headers: ");
-        Serial.println(err);
-      }
-    } else {
-      Serial.print("Getting response failed: ");
-      Serial.println(err);
-    }
-  } else {
-    Serial.print("Connect failed: ");
-    Serial.println(err);
-  }
-  http.stop();
+  int err = http.get("3.145.7.151", 5000, urlParam, NULL);
+  if (err != 0) Serial.println("Failed to send data.");
 }
