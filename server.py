d86ee7b4ab28e58ce49a8a1050098b162e05dbe4
Brianna Chizhevsky <bchizhev@uci.edu>
2024-12-13 22:36:10 -0800

    Changed steps to BPM. Updated javascript to fetch from /update_step every two seconds.

--- server.py
+++ server.py
@@ -1,7 +1,23 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask import request
 app = Flask(__name__)
 
+stats = {"temp": 0, "bpm": 0}
+
 @app.route("/")
 def hello():
-    return render_template("index.html", result = [request.args.get("temp"), request.args.get("steps")])
+    return render_template("index.html", result = [stats["temp"], stats["bpm"]])
+
+@app.route("/update_step", methods=["GET"])
+def update_step():
+    bpm = request.args.get("bpm")
+    temp = request.args.get("temp")
+    if bpm is not None:
+        stats["bpm"] = bpm 
+    else:
+        bpm = stats["bpm"] 
+    if temp is not None:
+        stats["temp"] = temp
+    else:
+        temp = stats["temp"]
+    return jsonify({"message": "Data updated successfully", "temp": temp, "steps": bpm})
